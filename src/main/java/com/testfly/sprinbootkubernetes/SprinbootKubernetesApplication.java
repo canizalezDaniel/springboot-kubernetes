c7b64994cebb6bd11222bd050a8cc43f8e1bbc4c
daniel <canizalezdaniel@gmail.com>
2019-09-06 15:32:02 -0400

    first upload code to test on GCP

---
+++ src/main/java/com/testfly/sprinbootkubernetes/SprinbootKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.testfly.sprinbootkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootKubernetesApplication.class, args);
+	}
+
+}
